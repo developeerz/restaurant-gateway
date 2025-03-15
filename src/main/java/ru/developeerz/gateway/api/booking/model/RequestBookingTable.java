daa07956668a9a2bd2fcf5e4570542ae2a0929a2
mikhail <mihaelvozhzhov@gmail.com>
2025-03-15 13:59:01 +0700

    TASK-1 Создал dockerfile и инструкцию ко сборке

--- src/main/java/ru/developeerz/gateway/api/booking/model/RequestBookingTable.java
+++ src/main/java/ru/developeerz/gateway/api/booking/model/RequestBookingTable.java
@@ -1,10 +1,16 @@
 package ru.developeerz.gateway.api.booking.model;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record RequestBookingTable(
+
         int userId,
+
+        @NotNull
         LocalDate date,
+
         int duration
 ) {
 }
